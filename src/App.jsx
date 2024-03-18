f9dfcd2fff12ec994e2ddb560958f319684c345b
shreyansh232 <shreyansh487@gmail.com>
2024-03-19 00:32:02 +0530

    Checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState(" ");
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)}
+           type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header"> To Do List</h1>
+      <ul className="list ">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
