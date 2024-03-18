c424282b04e13df31ac954929390439efb9f7fc5
shreyansh232 <shreyansh487@gmail.com>
2024-03-19 01:36:15 +0530

    Final 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from "react";
-import "./styles.css";
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewToDoForm"
+import "./styles.css"
+import { TodoList } from "./ToDoList"
 
 export default function App() {
-  const [newItem, setNewItem] = useState(" ");
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)}
-           type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-      <h1 className="header"> To Do List</h1>
-      <ul className="list ">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
-  );
+  )
 }
